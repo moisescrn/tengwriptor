639161cb6e0de9e259e1abd51af1de2537733121
moisescrn <moisescarbofuster@hotmail.com>
2026-03-05 13:11:34 +0100

    Correct and final implementation of the vowels. Now there is no trouble if a vowel appears more than once or at the beginning of the word or single as one word

--- Quenya/vowel_preparation.py
+++ Quenya/vowel_preparation.py
@@ -10,7 +10,7 @@
 non_accent = {"a", "e", "i", "o", "u"}
 accent = {"á", "é", "í", "ó", "ú"}
 diphtongs = {"ai", "oi", "ui", "iu", "eu", "au"}
-not_aara_vowels = {"é", "ó", "ú"}
+doubled_acc_vowels = {"é", "ó", "ú"}
 
 vowels = non_accent | accent
 
@@ -23,7 +23,7 @@ normalize = {
     "ú" : "u"
 }
 
-def manipulation_checker(word, vowel):
+def manipulation_checker(word, vowel, position):
     """
     Checks if a specific vowel needs a manipulation or if it can be let as it is
     There are two cases where manipulation is needed:
@@ -34,51 +34,83 @@ def manipulation_checker(word, vowel):
         True, manipulation needed
         False, no manipulation needed
     """
-    position = word.index(vowel)
-
+    position = int(position)
     if position == 0: # beginning, solves it for any vowel at the beginning, including with accent
         if vowel+word[1] not in diphtongs:
             return True
 
     elif word[position-1] in vowels and word[position-1]+vowel not in diphtongs: # after another vowel and not in diphtong
         return True
-    
-    if vowel in accent and vowel not in not_aara_vowels: # in case we always need an aara
+
+    elif vowel in accent and vowel not in doubled_acc_vowels: # in case we always need an aara
         return True
 
     return False
 
-def extract_vowels(word):
+def find_position(word):
     """
-    Returns in a list all vowels that appear in the input word
+    Returns a list with the position of the vowels
+    e.g.: for "ancalime" we have the vowels ['a', 'a', 'i', 'e']
+    and this function returns posit_vow = [0, 3, 5, 7]
     """
-    list_vow = []
-    for letter in word:
-        if letter in vowels:
-            list_vow.append(letter)
-    return list_vow
+    posit_vow = [position for position, letter in enumerate(word) if letter in vowels ]
+    return posit_vow
+
+def add_telco_aara(word, break_points):
+    """
+    telco and aara are lists that contain the position of the letters that need a telco or an aara to be added (so it must be added before that position)
+    This function takes word and adds the needed manipulations
+    """
+    break_points = sorted(break_points) # it should be sorted, but let's go safe
+    parts = []
+
+    # Split the word into parts: Whenever we need a telco or aara, we make a break
+    parts.append(word[:min(break_points)]) # first part, until the first vowel that needs manipulation
+    for j in range(1, len(break_points)): # begin with 1 to avoid an empty string
+        parts.append(word[break_points[j-1]:break_points[j]])
+    parts.append(word[max(break_points):])
+
+    # Now on each braek we add either a telco or an aara
+    # Note that each part, except the first one, begins with the vowel to be manipulated
+    new_parts = [parts[0]]
+    parts.pop(0) # eliminate the first part to allow the following iteration
+    for part in parts:
+        if part[0] in non_accent:
+            new_parts.append("|" + part)
+        elif part[0] in accent:
+            norm_vow = normalize[part[0]]
+            new_parts.append("^" + norm_vow + part[1:]) # eliminate the vowel with accent and then add it
+
+    # Sum all parts back together
+    manip_word = ""
+    for part in new_parts:
+        manip_word += part
+
+    return manip_word
 
 def vowel_preparation(word):
     """
     If needed the telco and aara characters are introduced
     """
-    list_vow = extract_vowels(word)
+    # Check if the word consists of only one character (always a vowel). if so then return the result
+    if len(word) == 1:
+        if word in non_accent:
+            return "|" + word
+        elif word in accent:
+            return "^" + normalize[word]
 
-    for vowel in list_vow:
-        if manipulation_checker(word, vowel) == True:
-            if vowel in non_accent:
-                word = word.replace(vowel, "|" + vowel)
-            elif vowel in accent:
-                word = word.replace(vowel, "^" + normalize[vowel])
+    posit_vow = find_position(word)
 
+    # take track of the positions where we need a break to add a telco or an aara
+    break_points = []
+    for position in posit_vow:
+        vowel = word[position]
+        if manipulation_checker(word, vowel, position) == True:
+            break_points.append(position)
+
+    if break_points == []: # it could be possible that we have no break points, so just return the original word
+        return word
+
+    # add the necessary manipulation and then just return the final word
+    word = add_telco_aara(word, break_points)
     return word
-
-if __name__ == "__main__":
-    word = input("write a word ")
-    vowel = input("write a vowel ")
-    done_result = vowel_preparation(word)
-    list_result = extract_vowels(word)
-    result = manipulation_checker(word, vowel)
-    print(result)
-    print(list_result)
-    print(done_result)
