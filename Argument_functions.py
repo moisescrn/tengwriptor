33c2dec5cb3695ad05e75a9d2a7782000a5a3ef1
moisescrn <moisescarbofuster@hotmail.com>
2026-03-14 18:28:58 +0100

    Imporvement of the text_transcriber script. For the moment it has all feature I wanted to implement. When more languages are aviable, this will be set in this script

---
+++ Argument_functions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/venv python3
+#
+# Defines the functions that are used in text_transcriber to allow the script
+# to take arguments, to modify the script behaviour
+
+import sys
+
+def helpPanel():
+    print("\nNAME")
+    print("\ttext_transcriber.py - applies the transcription from lating alphabet to tengwar\n")
+    print("DESCRIPTION")
+    print("\tThis script is responsible to apply the transcription to tengwar, changing the mode as needed depending on the language. The output is a .tex-file that has to be compiled to get the transcription\n")
+    print("OPTIONS")
+    print("\tThe script accepts the following options:\n")
+    print("\t-h, --help \t\t Prints this information panel")
+    print("\t-c, --compile \t\t The output .tex-file is compiled automatically")
+    print("\t-f, --font \t\t The font can be chosen arbitrary. The default font is 'annatar', in case the given one is invalid, or none is given")
+    print("\t-o, --outputFile \t Defines the name of the output file. The default name will be the name of the input file, interchanging the ending by '.tex'")
+    print("\t-l, --language \t\t Set the language in which the text is written, so the transcription can be performed correctly. The default language is Quenya\n")
+
+def set_font(font):
+    """
+    Checks if the font given is valid. If not the default font 'anatar' will be set
+    """
+    fonts = {"annatar", "annataritalic", "annatarbold", "annatarbolditalic",
+             "noldor", "noldorcapI", "noldorcapII",
+             "quenya", "quenyacapI", "quenyacapII",
+             "sindarin", "sindarincapI", "sindarincapII",
+             "teleri", "formal",
+             "parmaite", "unicodeparmaite",
+             "elfica", "gothika"}
+    if font not in fonts:
+        print("Invalid font, annatar will be used")
+        return "annatar"
+    else:
+        return font
+
+def set_outputfile(file_name):
+    """
+    Prints the name of the output file
+    """
+    return file_name
+
+def set_language(language):
+    """
+    Checks and sets the language.
+    For the moment there is only one language, so lets just print the name
+    In the future this will be changed
+    """
+    languages = {"quenya", "sindarin", "spanish", "english"}
+
+    if language not in languages:
+        print("Invalid language, quenya will be used")
+        return "quenya"
+    else:
+        return language
--- text_transcriber.py
+++ text_transcriber.py
@@ -1,17 +1,69 @@
-#!/usr/bin/ven python3
+#!/usr/bin/venv python3
 # 
 # Takes a file as input and generates a .tex file that can be compiled to give the transcribed version
-# https://stackoverflow.com/questions/22846858/python-pass-arguments-to-a-script
 
-import sys
+import getopt, sys, os
 from Quenya import word_transcriber
 from Splitter import words_splitter
+import Argument_functions
 
-#text_to_transcribe = sys.argv[0]
+# ---- Setting arguments ----
+#   -h ... help
+#   -c ... autocompile to get pdf automatically
+#   -f ... font
+#   -o ... name of output file
+#   -l ... language
 
-splitted_text = words_splitter.splitter("text.txt")
-file = open("tran.tex","w")
-file.write("\\documentclass{article}\n\\usepackage[annatar]{tengwarscript}\n\\pdfmapfile{=tengwarscript.map}\n\\pagenumbering{gobble}\n\\begin{document}\n")
+options = "hcf:o:l:"
+long_options = {"help", "compile", "font", "outputFile", "language"}
+
+# Chivatos, to see if an argument has been set
+comp_chiv = 0
+font_chiv = 0
+output_chiv = 0
+
+try:
+    arguments, values = getopt.getopt(sys.argv[1:], options, long_options)
+    for currentArg, currentVal in arguments:
+        if currentArg in {"-h", "--help"}:
+            Argument_functions.helpPanel()
+            sys.exit(1)
+        elif currentArg in {"-c", "--compile"}:
+            comp_chiv += 1
+        elif currentArg in {"-f", "--font"}:
+            font = Argument_functions.set_font(currentVal)
+            font_chiv += 1
+        elif currentArg in {"-o", "--outputFile"}:
+            new_file = Argument_functions.set_outputfile(currentVal)
+            output_chiv += 1
+        elif currentArg in {"-l", "--language"}:
+            Argument_functions.set_language(currentVal)
+except getopt.GetoptError as err:
+    print(str(err))
+    print("\nPrinting usage instructions:")
+    Argument_functions.helpPanel()
+    sys.exit(1)
+
+# ------- Default settings -------
+# in case some arguments are not given, explicitly we need a default setting
+text_to_transcribe = sys.argv[-1]
+if font_chiv == 0:
+    font = "annatar"
+if output_chiv == 0:
+    if "." in text_to_transcribe:
+        new_file = text_to_transcribe[:-4] + ".tex"
+    else:
+        new_file = text_to_transcribe + ".tex"
+# fot the future
+#if lang_chiv == 0:
+#    language = "quenya"
+
+# ---- Reading and splitting text ----
+splitted_text = words_splitter.splitter(text_to_transcribe)
+
+# ---- Trascription ----
+file = open(new_file,"w")
+file.write("\\documentclass{article}\n\\usepackage[" + font + "]{tengwarscript}\n\\pdfmapfile{=tengwarscript.map}\n\\pagenumbering{gobble}\n\\begin{document}\n")
 
 paragraph = ""
 for paragr_num in range(len(splitted_text)):
@@ -24,3 +76,7 @@ for paragr_num in range(len(splitted_text)):
 
 file.write("\n\\end{document}")
 file.close()
+
+# If -c has been set, lets compile it
+if comp_chiv == 1:
+    os.system(f"pdflatex {new_file}")
