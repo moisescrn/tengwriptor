50886c7f3980f6f569e53e93677197de384285a1
moisescrn <moisescarbofuster@hotmail.com>
2026-03-23 12:35:34 +0100

    Correction of a little bug in the spanish tengwriptor: que and qui did not always work, because it transcribed ue and ui as diphtongs

--- Spanish/special_cases_converter.py
+++ Spanish/special_cases_converter.py
@@ -4,12 +4,15 @@
 #
 # This file contains functions that convert the clusters into other (unique) characters to make the final conversion better
 
-from .letters import clusters, cluster_dict
+from .letters import consonant_clusters, clusters, cluster_dict
 
 def cluster_converter(word):
     """
     Apply the map defined by the dictionary
     """
+    # Make firstly the consonants to avoid problems with que and qui
+    for cluster in consonant_clusters:
+        word = word.replace(cluster, cluster_dict[cluster])
     for cluster in clusters:
         word = word.replace(cluster, cluster_dict[cluster])
     return word
